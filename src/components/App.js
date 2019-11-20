266e570a12d653d84e8a2c3df5db8bfae830c951
Ryan Mehta <ryan.mehta@gmail.com>
2019-11-20 11:56:45 -0800

    signup/signin

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import SignupUser from '../containers/SignupUser';
+import LoginUser from '../containers/LoginUser';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h2>Signup?</h2>
+      <SignupUser />
+      <h2>Or Login</h2>
+      <LoginUser />
+    </>
+  );
 }
   
