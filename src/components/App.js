8fc217631f022fa4919fd9584c80e598082ddf1c
Ryan Mehta <ryan.mehta@gmail.com>
2019-11-21 10:22:14 -0800

    add private route

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,50 @@
-import React from 'react';
-import SignupUser from '../containers/SignupUser';
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import LoginUser from '../containers/LoginUser';
+import { getSessionId, getSessionLoading } from '../selectors/sessionSelectors';
+import { sessionVerify } from '../actions/sessionActions';
+
+const Home = () => (
+  <h1>Home</h1>
+);
+
+const PrivateRoute = ({ ...rest }) => {
+  const sessionId = useSelector(getSessionId);
+  const loading = useSelector(getSessionLoading);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if(!sessionId) dispatch(sessionVerify());
+  }, []);
+
+  // logged out
+  if(loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if(!loading && !sessionId) {
+    // wait to hear back
+    return <Redirect to="/login" />;
+  }
+
+  // redirect to login page
+  // logged in
+  return <Route {...rest} />;
+};
 
 export default function App() {
   return (
-    <>
-      <h2>Signup?</h2>
-      <SignupUser />
-      <h2>Or Login</h2>
-      <LoginUser />
-    </>
+    <Router>
+      <Switch>
+        <PrivateRoute exact path="/" component={Home} />
+        <Route path="/login" component={LoginUser} />
+      </Switch>
+    </Router>
   );
 }
   
--- src/components/user/UserForm.js
+++ src/components/user/UserForm.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getSessionId } from '../../selectors/sessionSelectors';
 
 const UserForm = ({ buttonText, handleSubmit }) => {
+  const sessionId = useSelector(getSessionId);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  if(sessionId) return <Redirect to="/" />;
 
   return (
     <form onSubmit={event => handleSubmit(event, username, password)}>
